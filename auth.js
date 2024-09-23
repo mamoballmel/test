321bc61e3ad2ecbf8b0d1179a494237e1d970b76
mamoballmel <ronplay300@gmail.com>
2024-09-23 09:29:10 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -73,7 +73,7 @@ registerForm.addEventListener('submit', (e) => {
 
         // Перенаправление на главную страницу через 2 секунды
         setTimeout(() => {
-            window.location.href = 'betting.html';
+            window.location.href = '/betting.html';
         }, 2000);
     })
     .catch((error) => {
