be427cd2f013fc37be2e8689c5a52a8b3baf3928
mamoballmel <ronplay300@gmail.com>
2024-09-24 09:26:44 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,13 +1,24 @@
-  document.getElementById('createPageForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-    
-    const response = await fetch('/create-page', {
-      method: 'POST',
-      body: formData
-    });
-
-    const result = await response.json();
-    alert(result.message);
-  });
+        document.getElementById('createPageForm').addEventListener('submit', function (event) {
+            event.preventDefault(); // предотвращаем стандартное поведение формы
+            
+            const formData = new FormData(this); // собираем данные формы
+            
+            fetch(this.action, {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Ошибка отправки формы');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                alert('Форма успешно отправлена!');
+            })
+            .catch(error => {
+                console.error('Ошибка:', error);
+                alert('Произошла ошибка при отправке формы.');
+            });
+        });
